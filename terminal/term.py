da53f9317e701b342101219b2c1768155b1a0dba
YourWonders <legendofroland1@gmail.com>
2026-09-06 23:08:43 +0200

    initial ideas, similar to pash project but more in relation to bash itself

--- terminal/term.py
+++ terminal/term.py
@@ -1,1 +1,42 @@
-print("hello world")
+import sys, logging
+
+
+lg = logging.basicConfig(level=logging.INFO,
+                         format="""
+LEVEL => %(levelname)s
+INFORMATION => %(message)s
+TIME OF INCIDENT => %(asctime)s
+"""
+)
+
+LOW_WARNING = 25
+logging.addLevelName(LOW_WARNING, 'LOW WARNING')
+
+
+
+s1 = sys.argv[1]
+cmd = {'read_file':'-rd'}
+
+
+# ------------------------------------------------
+def function():
+
+    path_arg = sys.argv[2]
+
+
+    with open(path_arg,'r') as flr:
+
+        data = flr.readlines()
+
+        for info in data:
+
+            print(info)
+
+        flr.close()
+
+
+if s1 == cmd['read_file']:
+
+    function()
+
+# gonna fix up some ideas later...
